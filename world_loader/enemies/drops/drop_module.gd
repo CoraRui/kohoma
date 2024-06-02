32fe4290f3f6063152cd3cfb89541f8c6ff142fe
cora rui <coraruidev@gmail.com>
2024-06-02 14:02:10 -0700

    reorganized enemy assets, may have deleted some original aseprite files in the process. :I

--- world_loader/enemies/drops/drop_module.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-#this script should contain a few possibilities for drops on death, and how to select which will be dropped. for now its just random.
-
-
-#I think this was replaced by simple drop? but simple drop might specifically be for just normal guaranteed drops. anyways probably delete this later.
---
+++ world_loader/enemies/drops/drop_module/drop_module.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+#TODO: decide if you want to keep this and the simple drop thing separate
+#TODO: add weighted randomized drops
+
+
+#this script should contain a few possibilities for drops on death, and how to select which will be dropped. for now its just random.
+
+
+#I think this was replaced by simple drop? but simple drop might specifically be for just normal guaranteed drops. anyways probably delete this later.
